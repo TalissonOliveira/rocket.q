f0904d89e77ca1530f3e208e9ce32e63eadab597
Talisson <30274736+TalissonOliveira@users.noreply.github.com>
2021-07-22 03:32:48 -0300

    add room to database

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -5,23 +5,32 @@ module.exports = {
         const db = await Database()
         const pass = req.body.password
         let roomId
-
-        for(let i = 0; i < 6; i++) {
-            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
-            roomId += Math.floor(Math.random() * 10).toString()
+        let isRoom = true
+        while (isRoom) {
+            // Create room id
+            for (let i = 0; i < 6; i++) {
+                i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+                roomId += Math.floor(Math.random() * 10).toString()
+            }
+    
+            // Check if the id exists
+            const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+    
+            isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId)
+    
+            if (!isRoom) {
+                // Insert room into database
+                await db.run(`
+                    INSERT INTO rooms (
+                        id,
+                        pass
+                    ) VALUES (
+                        ${parseInt(roomId)},
+                        ${pass}
+                    )
+                `);
+            }
         }
-
-        console.log(parseInt(roomId), pass)
-
-        await db.run(`
-            INSERT INTO rooms (
-                id,
-                pass
-            ) VALUES (
-                ${parseInt(roomId)},
-                ${pass}
-            )
-        `);
 
         await db.close()
         
